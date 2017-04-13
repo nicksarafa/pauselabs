966c4efe0b69f237cc683edb14843079c4ea6c14
nsarafa <nicksarafa@gmail.com>
2017-04-13 04:25:49 -0400

    tests init

---
+++ test/config.test.js
@@ -0,0 +1,10 @@
+const path = require(`path`)
+
+module.exports = {
+  test: `Export app module`,
+  cwd: path.resolve(path.join(__dirname, '../')),
+  cmd: `node`,
+  args: [`app.js`],
+  port: 8081,
+  msg: `Hello, world!`
+}
