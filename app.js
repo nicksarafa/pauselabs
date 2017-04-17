9629d6c9e496b3abb4ff2a1d1f3099d3e90277bb
nsarafa <nicksarafa@gmail.com>
2017-04-16 20:51:19 -0400

    update startup script && delete redundant entry point

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 if (module === require.main) {
     const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port
-    console.log('App listening on port ${port}')
+    console.log('App listening on port', port)
   })
 }
 
