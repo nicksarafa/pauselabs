cd6a61c07c205338e24541b68e067da3c5265f05
Nick Sarafa <nicksarafa@gmail.com>
2018-01-16 17:57:48 +0800

    trust proxy, force production https redirects && prettierc init

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const path = require('path')
+const https = require('https')
 
 const app = express()
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.enable('trust proxy')
+
+app.use(function(req, res, next) {
+  if (!req.secure && process.env.NODE_ENV === "production") {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''))
+  }
+  next()
+}, express.static(path.join(__dirname, 'public')))
 
 if (module === require.main) {
-    const server = app.listen(process.env.PORT || 8080, () => {
+  const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port
-    console.log('App listening on port', port)
+    if (process.env.NODE_ENV !== "production") {
+      console.log('App listening on port', port)
+    }
   })
 }
 
